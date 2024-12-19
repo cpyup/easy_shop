ede6e808709258ec88f8f8d378be3f7502fa9585
Cameron <cpolly@pgh.yearup.org>
2024-12-18 19:38:32 -0500

    Implemented Adjusting Current Stock With Purchase

--- EasyShop/src/main/java/org/yearup/controllers/OrdersController.java
+++ EasyShop/src/main/java/org/yearup/controllers/OrdersController.java
@@ -13,9 +13,7 @@ import org.yearup.models.*;
 
 import java.math.BigDecimal;
 import java.security.Principal;
-import java.sql.Date;
 import java.sql.Timestamp;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,14 +28,16 @@ public class OrdersController {
     private final UserDao userDao;
     private final ProfileDao profileDao;
     private final OrderDao orderDao;
+    private final ProductDao productDao;
 
     @Autowired
-    public OrdersController(OrderLineItemDao orderLineItemDao, ShoppingCartDao shoppingCartDao, UserDao userDao, ProfileDao profileDao, OrderDao orderDao) {
+    public OrdersController(OrderLineItemDao orderLineItemDao, ShoppingCartDao shoppingCartDao, UserDao userDao, ProfileDao profileDao, OrderDao orderDao, ProductDao productDao) {
         this.orderLineItemDao = orderLineItemDao;
         this.shoppingCartDao = shoppingCartDao;
         this.userDao = userDao;
         this.profileDao = profileDao;
         this.orderDao = orderDao;
+        this.productDao = productDao;
     }
 
     @PostMapping
@@ -63,22 +63,42 @@ public class OrdersController {
                 setShippingAmount(BigDecimal.ZERO);
             }};
 
-            order = orderDao.create(order);
+
             ShoppingCart cart = shoppingCartDao.getByUserId(user.getId());
 
             List<ShoppingCartItem> cartItems = new ArrayList<>(cart.getItems().values());
+            List<Product> productList = new ArrayList<>();
+
+            for(ShoppingCartItem cartItem: cartItems){
+                Product p = productDao.getById(cartItem.getProductId());
+                productList.add(p);
+
+                if(cartItem.getQuantity() > p.getStock() || cartItem.getQuantity() < 1){
+                    throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, p.getStock() + " Items Currently In Stock");
+                }
+            }
+
+            order = orderDao.create(order);
 
             for (ShoppingCartItem cartItem : cartItems) {
                 OrderLineItem orderItem = convertToLineItem(cartItem, order);
                 orderItem = orderLineItemDao.create(orderItem);
                 order.add(orderItem);
+
+                // Update the current inventory
+                for(Product p : productList){
+                    if(p.getProductId() == cartItem.getProductId()){
+                        p.setStock(p.getStock() - cartItem.getQuantity());
+                        productDao.update(p.getProductId(),p);
+                    }
+                }
             }
 
             shoppingCartDao.emptyCart(user.getId());
             return order;
 
         } catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.", e);
         }
     }
